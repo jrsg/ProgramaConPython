270daa50b3e42db3c05943a63a4213b105e2cb70
Ruben Sanchez <rsanchez@ucc.mx>
2022-04-05 08:43:06 -0500

    Ordenamiento de ganadores

--- Unidad4/ejercicio 4.8/main.py
+++ Unidad4/ejercicio 4.8/main.py
@@ -1,6 +1,9 @@
 import random
 #  Genera una lista con 5 números aleatorios entre 1 y 15
 ganadores = random.sample(range(1, 15), 5)
+#  Ordenar ascendentemente
+ganadores.sort()
 jugador = []
+
 #  A continuación escribe tu solución
 
